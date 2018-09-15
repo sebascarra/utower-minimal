9575739f00ef0eefabd1b4d36e4808fb2f088ced
Julian Mateu <jmateu@google.com>
2018-09-15 20:44:38 -0300

    WIP: refactor of peristaltic pumps to use better architecture.

--- peristaltic.py
+++ peristaltic.py
@@ -8,57 +8,99 @@ from time import sleep
 import RPi.GPIO as GPIO
 import sys
 
-pumpName = sys.argv[1]
+class ComputerBoard(object):
 
-# Next we setup the pins for use!
-GPIO.setmode(GPIO.BCM)
-#GPIO.setwarnings(False)
+  @staticmethod
+  def Init():
+    GPIO.setmode(GPIO.BCM)
 
-pumps = {'P1': (18, 25), #Dictionary
-         'WP': (22, 22),
-         'P2': (27, 12),
-         'P3': (16, 20)
-}
+  @staticmethod
+  def SetOutputToPin(pin_number, state) 
+    GPIO.output(pin_number, state)
 
-try:
-  print('GPIOs: ' + str(pumps[pumpName]))
+  @staticmethod
+  def InitializePinAsOutput(pin_number):
+    GPIO.setup(pin_number, GPIO.OUT)
 
-  pin1 = pumps[pumpName][0]
-  pin2 = pumps[pumpName][1]
+
+class DeviceManager(object):
   
-  GPIO.setup(pin1, GPIO.OUT)
-  GPIO.setup(pin2, GPIO.OUT)
+  pumps = {'P1': (18, 25), #Dictionary
+           'WP': (22, 22),
+           'P2': (27, 12),
+           'P3': (16, 20)
+  }
 
-  GPIO.output(pin1, False)
-  GPIO.output(pin2, False)
+  # TODO: create static array of pumps.
+  
+  @staticmethod
+  def Init():
+    # TODO: add other pumps from dictionary
+    # TODO: add new pump to the static array.
+    pump1 = PeristalticPump('P1') 
+    pin1 = pumps[pump1.name][0]
+    pin2 = pumps[pump1.name][1]
+    ComputerBoard.InitializePinAsOutput(pin1)
+    ComputerBoard.InitializePinAsOutput(pin2)
+    StopPump(pump1)
 
-  print('Starting motor sequence!')
+  
+  @staticmethod
+  def StartPump(persitaltic_pump): 
+    
+    pin1 = pumps[peristaltic_pump.name][0]
+    pin2 = pumps[peristaltic_pump.name][1]
 
-  while True:
-    try:
-      # Makes the motor spin one way for 3 seconds
-      GPIO.output(pin1, False)
-      GPIO.output(pin2, True)
-      sleep(3)
-      # Idle for a sec
-      GPIO.output(pin1, False)
-      GPIO.output(pin2, False)
-      sleep(1)
-      # Spins the other way for a further 3 seconds
-      #GPIO.output(pin1, False)
-      #GPIO.output(pin2, True)
-      #sleep(3)
-      # Idle for a sec
-      #GPIO.output(pin1, False)
-      #GPIO.output(pin2, False)
-      #sleep(1)
-    except(KeyboardInterrupt):
-      # If a keyboard interrupt is detected then it exits cleanly!
-      print('Finishing up!')
-      GPIO.output(pin1, False)
-      GPIO.output(pin2, False)
-      quit()
-except:
-  print('Pump does not exist.')
+    ComputerBoard.SetOutputToPin(pin1, False)
+    ComputerBoard.SetOutputToPin(pin2, True)
+
+  @staticmethod
+  def StopPump(persitaltic_pump): 
+    
+    pin1 = pumps[peristaltic_pump.name][0]
+    pin2 = pumps[peristaltic_pump.name][1]
+
+    ComputerBoard.SetOutputToPin(pin1, False)
+    ComputerBoard.SetOutputToPin(pin2, False)
+
+
+class PeristalticPump(object):
+
+  def _init_(self, name):
+    self._name = name
+    self._is_on = False
+    
+  def Start():
+    #TODO: add direction
+    DeviceManager.StartPump(self)
+    self._is_on = True
+
+  def Stop():
+    DeviceManager.StopPump(self)
+    self._is_on = False
+
+def main():
+
+  pumpName = sys.argv[1]
+
+  try:
+    DeviceManager.Init() 
+    print('Starting motor sequence!')
+  
+    while True:
+      try:
+        # TODO: ask device manager to start and stop pumps
+        
+        #Start pump
+        sleep(3)
+        # Stop pump
+        sleep(1)
+      except(KeyboardInterrupt):
+        # TODO: ask device manager to stop pumps
+        # If a keyboard interrupt is detected then it exits cleanly!
+        print('Finishing up!')
+        quit()
+  except:
+    print('Pump does not exist.')
 
 
